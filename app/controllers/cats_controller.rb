86ee0367f6cca35658701c771b147575280ab493
Jason&Arthur <jasonarthur@appacademy.io>
2015-08-18 14:54:32 -0400

    finish everthing for Cat model

---
+++ app/controllers/cats_controller.rb
@@ -0,0 +1,39 @@
+class CatsController < ApplicationController
+  def index
+    @cats = Cat.all
+    render :index
+  end
+
+  def show
+    @cat = Cat.find(params[:id])
+    render :show
+  end
+
+  def new
+    @cat = Cat.new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      redirect_to cat_url(@cat)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @cat = Cat.find(params[:id])
+  end
+
+  def update
+    @cat = Cat.find(params[:id])
+    @cat.update(cat_params)
+    redirect_to cat_url(@cat)
+  end
+
+  private
+  def cat_params
+    params.require(:cat).permit(:name, :birth_date, :sex, :description, :color)
+  end
+end
---
+++ app/models/cat.rb
@@ -0,0 +1,15 @@
+class Cat < ActiveRecord::Base
+  validates :birth_date, :color, :name, :sex, presence: true
+  validates :sex, inclusion: %w(M F)
+  validates :color, inclusion: %w(orange black white gray combo)
+
+  def age
+    # (Date.today.year - birth_date.year)
+    now = Time.now.utc.to_date
+    now.year - birth_date.year - (birth_date.to_date.change(:year => now.year) > now ? 1 : 0)
+  end
+
+  def age_in_years
+    age.year
+  end
+end
